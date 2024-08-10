80b3ecb57c4b630fc47219eef1b759015ac49841
MaouStan <zazalnm4@gmail.com>
2024-08-10 09:31:25 +0700

    update contect

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -15,15 +15,15 @@ const Hero = () => (
       }
       description={
         <>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            malesuada
-          </a>{' '}
-          nisi tellus, non imperdiet nisi tempor at. Lorem ipsum dolor sit amet,{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            consectetur
-          </a>{' '}
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+          Hi, I'm Maoustan, Software Developer from Thailand
+          <br />
+          🔬 I'm currently studying for my bachelor at Mahasarakham University
+          <br />
+          💻 I love writing code and learn anything about it
+          <br />
+          📚 I’m currently learning Game Hacking
+          <br />
+          💬 Ask me anything about from Here
         </>
       }
       avatar={
@@ -36,28 +36,34 @@ const Hero = () => (
       }
       socialButtons={
         <>
-          <a href="/">
+          {/* <a href="/">
             <HeroSocial
               src="/assets/images/twitter-icon.png"
               alt="Twitter icon"
             />
-          </a>
-          <a href="/">
+          </a> */}
+          <a href="https://web.facebook.com/zazalnm223/">
             <HeroSocial
               src="/assets/images/facebook-icon.png"
               alt="Facebook icon"
             />
           </a>
-          <a href="/">
+          {/* <a href="/">
             <HeroSocial
               src="/assets/images/linkedin-icon.png"
               alt="Linkedin icon"
             />
-          </a>
-          <a href="/">
+          </a> */}
+          <a href="https://www.youtube.com/@maoustan">
             <HeroSocial
               src="/assets/images/youtube-icon.png"
               alt="Youtube icon"
+            />
+          </a>
+          <a href="https://www.instagram.com/maoustan">
+            <HeroSocial
+              src="/assets/images/instagram-icon.png"
+              alt="Instagram icon"
             />
           </a>
         </>
